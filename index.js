1f855ff72c05b71ae9692cc3d2bb0f18542cdf80
Ian DesJardins <ian.d.desjardins@gmail.com>
2024-01-24 00:56:11 -0500

    chore: Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var spellLevelMap = {
+  "Cantrip": 0,
+  "1st": 1,
+  "2nd": 2,
+  "3rd": 3,
+  "4th": 4,
+  "5th": 5,
+  "6th": 6,
+  "7th": 7,
+  "8th": 8,
+  "9th": 9,
+};
+
+var spellSchoolToIconMap = {
+  "Abjuration": "fa-shield-alt",
+  "Conjuration": "fa-hand-sparkles",
+  "Divination": "fa-eye",
+  "Enchantment": "fa-magic",
+  "Evocation": "fa-fire",
+  "Illusion": "fa-mask",
+  "Necromancy": "fa-skull",
+  "Transmutation": "fa-exchange-alt",
+};
